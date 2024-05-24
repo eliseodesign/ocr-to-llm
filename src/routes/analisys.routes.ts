5ed0459e686eebea619b46de23d9e3c010e39207
Eliseo Arévalo <eliseoarevalo29@gmail.com>
2024-05-24 06:36:42 -0600

    feat: :sparkles: add counter to limit request

--- src/routes/analisys.routes.ts
+++ src/routes/analisys.routes.ts
@@ -1,12 +1,13 @@
-// rutas de analisis
 import { Router, Request, Response } from 'express';
 import { envConfig } from '../shared/config/env'
 import { DocumentAnalysisService } from '../service/azure.services'
 import { MistralProvider} from '../service/mistral.services'
-
 import multer from 'multer';
-const upload = multer({ memoryStorage: multer.memoryStorage() });
+import fs from 'fs';
+import { config } from 'dotenv'
+config()
 
+const upload = multer({ memoryStorage: multer.memoryStorage() });
 const router = Router();
 
 router.post('/ocr', upload.single('image'), async (req: Request, res: Response, next) => {
@@ -17,19 +18,38 @@ router.post('/ocr', upload.single('image'), async (req: Request, res: Response, 
         if (!file) {
             return res.status(400).json({ error: 'No se recibió un archivo' });
         }
+
+        // Read the current counter value from the file
+        let counter = parseInt(fs.readFileSync('counter.txt', 'utf-8'));
         
-        const documentService = new DocumentAnalysisService(envConfig.AZURE_END, envConfig.AZURE_KEY);
-        const id = await documentService.analyzeDocument(file.buffer)
-    
-        const read = await documentService.readResult(id)
-        console.log("end read")
-        const mistral = new MistralProvider()
-        const result = await mistral.createCompletion(read.analyzeResult.content, true)
+        // Check if the counter is less than 10
+        if (counter < Number(process.env.LIMIT)) {
+            // Increment the counter by one
+            counter++;
+            
+            // Write the updated counter value back to the file
+            fs.writeFileSync('counter.txt', counter.toString(), 'utf-8');
+            
+            const documentService = new DocumentAnalysisService(envConfig.AZURE_END, envConfig.AZURE_KEY);
+            const id = await documentService.analyzeDocument(file.buffer)
         
-        const json = JSON.parse(result)
-        res.json(json);
+            const read = await documentService.readResult(id)
+            console.log("end read")
+            const mistral = new MistralProvider()
+            const result = await mistral.createCompletion(read.analyzeResult.content, true)
+            
+            const json = JSON.parse(result)
+            res.json({
+                counter: counter,
+                limit: Number(process.env.LIMIT),
+                mistral: json
+            });
+        } else {
+            res.status(400).json({ error: 'Limite alcanzado' });
+        }
     } catch (error) {
         next(error);
     }
 });
+
 export { router }
